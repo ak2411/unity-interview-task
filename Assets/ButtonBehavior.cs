c37d9e11952134c230ee5bfe453a8fd2eef23f33
Angelica Kosasih <angelicakosasih24@gmail.com>
2022-11-15 11:50:24 -0800

    Git and scene setup

---
+++ Assets/ButtonBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void SpawnObject()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
