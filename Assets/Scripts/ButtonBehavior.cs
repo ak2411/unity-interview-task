eb49c9d93af36dcc012231dcb7211b11ae4530af
Angelica Kosasih <angelicakosasih24@gmail.com>
2022-11-16 10:49:06 -0800

    Add drag and drop behavior. Rotate semi-functioning

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -8,6 +8,8 @@ public class ButtonBehavior : MonoBehaviour
     [SerializeField] private  GameObject m_spawnObjectPrefab;
     private const float SPAWNDISTANCE = 5f;
     private Camera m_mainCamera;
+
+    private GameObject m_currSpawnedObject;
     // Start is called before the first frame update
     void Awake()
     {
@@ -18,11 +20,28 @@ public class ButtonBehavior : MonoBehaviour
     {
         var cameraTransform = m_mainCamera.transform;
         var spawnPosition = cameraTransform.position + cameraTransform.forward * SPAWNDISTANCE;
-        Instantiate(m_spawnObjectPrefab, spawnPosition, Quaternion.identity);
+        m_currSpawnedObject = Instantiate(m_spawnObjectPrefab, GetWorldMousePos(), Quaternion.identity);
     }
 
+    private Vector3 GetWorldMousePos()
+    {
+        var mousePos =Input.mousePosition;
+        mousePos.z = m_mainCamera.WorldToScreenPoint(this.transform.position).z;
+        return  m_mainCamera.ScreenToWorldPoint(mousePos);
+    }
     private void OnMouseDown()
     {
         SpawnObject();
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!m_currSpawnedObject) return;
+        m_currSpawnedObject.transform.position = GetWorldMousePos();
+    }
+
+    private void OnMouseUp()
+    {
+        m_currSpawnedObject = null;
     }
 }
---
+++ Assets/Scripts/XScaleBehavior.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class XScaleBehavior : MonoBehaviour
+{
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
