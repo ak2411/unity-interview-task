b01516b059002f8e51f3432be46f719bcc5110ae
Angelica Kosasih <angelicakosasih24@gmail.com>
2022-11-16 23:34:38 -0800

    Add translate behavior to switch between axes

--- Assets/Scripts/ManipulationWidgetBehavior.cs
+++ Assets/Scripts/ManipulationWidgetBehavior.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class ManipulationWidgetBehavior : MonoBehaviour
 {
-      [SerializeField] private ManipulationDirection m_type;
+      [SerializeField] public ManipulationDirection m_type;
       private GameObject m_cone;
       private GameObject m_sphere;
       private Vector3 m_RotateAngle;
@@ -16,13 +16,14 @@ public class ManipulationWidgetBehavior : MonoBehaviour
             switch(m_type)
             {
                   case ManipulationDirection.X:
-                        m_RotateAngle = new Vector3(0f, 0f, -90f);
-                        break;
-                  case ManipulationDirection.Y:
                         m_RotateAngle = new Vector3(0f, 0f, 0f);
                         break;
-                  case ManipulationDirection.Z:
+                  case ManipulationDirection.Y:
                         m_RotateAngle = new Vector3(0f, -90f, -90f);
+
+                        break;
+                  case ManipulationDirection.Z:
+                        m_RotateAngle = new Vector3(-90f, 0f, -90f);
                         break;
                   default:
                         throw new ArgumentOutOfRangeException();
--- Assets/TranslateBehavior.cs
+++ Assets/TranslateBehavior.cs
@@ -1,18 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TranslateBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField] private Material m_selectedMaterial;
+   private Material m_unselectedMaterial;
+   private ManipulationWidgetBehavior.ManipulationDirection m_type;
+   private Camera m_mainCameraRef;
+   private Transform m_parentRef;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private Queue<Vector3> m_mousePositions = new Queue<Vector3>();
+   private float m_mouseZPos;
+   private Vector3 m_originalPos;
+
+   private void Awake()
+   {
+      if (!transform.parent.GetComponent<ManipulationWidgetBehavior>())
+      {
+         throw new Exception("Parent must have the ManipulationWidgetBehavior");
+      }
+
+      m_type = transform.parent.GetComponent<ManipulationWidgetBehavior>().m_type;
+      m_unselectedMaterial = GetComponent<MeshRenderer>().material;
+      m_parentRef = transform.parent.parent;
+      m_mainCameraRef = Camera.main;
+   }
+
+   private void OnMouseDown()
+   {
+      m_originalPos = transform.position;
+      m_mouseZPos = m_mainCameraRef.WorldToScreenPoint(m_parentRef.position).z;
+      GetComponent<MeshRenderer>().material = m_selectedMaterial;
+      m_mousePositions.Enqueue(_getWorldMousePos());
+   }
+
+   private Vector3 _getWorldMousePos()
+   {
+      var mousePos = Input.mousePosition;
+      mousePos.z = m_mouseZPos;
+      return m_mainCameraRef.ScreenToWorldPoint(mousePos);
+   }
+
+   private void OnMouseDrag()
+   {
+      m_mousePositions.Enqueue(_getWorldMousePos());
+   }
+
+   private void OnMouseUp()
+   {
+      GetComponent<MeshRenderer>().material = m_unselectedMaterial;
+   }
+
+   private void Update()
+   {
+      if (m_mousePositions.Count < 1) return;
+      var pos = m_mousePositions.Dequeue();
+      var targetPos = transform.position;
+      switch (m_type)
+      {
+         case ManipulationWidgetBehavior.ManipulationDirection.X:
+            targetPos.x = pos.x;
+            break;
+         case ManipulationWidgetBehavior.ManipulationDirection.Y:
+            targetPos.y = pos.y;
+            break;
+         case ManipulationWidgetBehavior.ManipulationDirection.Z:
+            targetPos.z = pos.y;
+            break;
+         default:
+            throw new ArgumentOutOfRangeException();
+      }
+
+      m_parentRef.position += (targetPos-m_originalPos);
+      m_originalPos = targetPos;
+   }
 }
